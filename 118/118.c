699965dc1df792e0080fd3205be4945fbc6f7244
KushagrJ <kushagr.j@gmail.com>
2021-08-02 10:52:45 +0530

    Update C-01

--- 118/118.c
+++ 118/118.c
@@ -10,18 +10,18 @@ int main(int argc, char ** argv)
     if (argc != 2)
         printf("Enter this: %s filename\n", argv[0]), exit(EXIT_FAILURE);
 
-    FILE * filePointer;
-    if ((filePointer = fopen(argv[1], "r")) == NULL)
+    FILE * fp;
+    if ((fp = fopen(argv[1], "r")) == NULL)
         printf("Can't open %s\n", argv[1]), exit(EXIT_FAILURE);
 
     int c;
     unsigned long count = 0;
-    while ((c = getc(filePointer)) != EOF)
+    while ((c = getc(fp)) != EOF)
         putc(c, stdout), count++;
 
-    fclose(filePointer);
+    fclose(fp);
 
-    printf("File %s has %lu characters\n", argv[1], count);
+    printf("\nFile %s has %lu characters\n", argv[1], count);
 
     return 0;
 
@@ -57,10 +57,11 @@ int main(int argc, char ** argv)
     when reading a file and convert \n to \r when writing to a file under the
     text mode.
     Similarly, for an MS-DOS platform, the conversions would be \r\n to \n and
-    \n to \r\n.]
+    \n to \r\n, respectively.]
 
  * C programs automatically open 3 files on the user's behalf: the standard
    input, the standard output and the standard error output.
+   The file pointers associated with these files are stdin, stdout and stderr.
    The standard input, by default, is the normal input device for a system,
    usually the keyboard.
    The standard output and the standard error output, by default, are the normal
@@ -74,18 +75,55 @@ int main(int argc, char ** argv)
  * The exit() function also closes any open files before terminating a program.
 
  * The fopen() function, prototyped in stdio.h, is used to open files.
-   Its first argument is the address of a string containing the name of the file
-   to be opened. Its second argument is a string identifying the mode in which
-   the file is to be opened.
+   Its first argument is the address of the string containing the name of the
+   file to be opened. Its second argument is a string (or more accurately, the
+   string's address) identifying the mode in which the file is to be opened.
  * Different mode strings for fopen() :-
-   1. "r"    - Open a text file for reading.
-      "rb"   - Same, but in binary mode instead of text mode.
-   2. "w"    - Open a text file for writing, truncating an existing file to zero
-               length, or creating the file if it does not exist.
-      "wb"   - Same, but in binary mode instead of text mode.
-   3. "a"    - Open a text file for writing, appending to the end of an existing
-               file, or creating the file if it does not exist.
-      "ab"   - Same, but in binary mode instead of text mode.
-   4. "r+"   - Open a text file for update (i.e. reading and writing both).
+   1. "r"   - Open a text file for reading. The file must exist.
+      "rb"  - Same, but in binary mode instead of text mode.
+   2. "w"   - Open a text file for writing, truncating an existing file to zero
+              length or creating the file if it does not exist.
+      "wb"  - Same, but in binary mode instead of text mode.
+   3. "a"   - Open a text file for writing, appending to the end of an existing
+              file or creating the file if it does not exist.
+      "ab"  - Same, but in binary mode instead of text mode.
+   4. "r+"  - Open a text file for update (i.e. reading and writing both). The
+              file must exist.
+      "r+b" - Same, but in binary mode instead of text mode.
+      "rb+" - Same as "r+b".
+   5. "w+"  - Open a text file for update (i.e. reading and writing both),
+              first truncating the file to zero length if it exists or creating
+              the file if it does not exist.
+      "w+b" - Same, but in binary mode instead of text mode.
+      "wb+" - Same as "w+b".
+   6. "a+"  - Open a text file for update (i.e. reading and writing both),
+              appending to the end of an existing file or creating the file if
+              it does not exist. The whole file can be read, but writing can
+              only be appended.
+      "a+b" - Same, but in binary mode instead of text mode.
+      "ab+" - Same as "a+b".
+   [C11 offers additional modes, such as "wx", "wbx", "w+x", "w+bx", etc., which
+    are like the corresponding non-x modes, except they fail if the file already
+    exists (instead of truncating it to zero length) and they open a file in
+    exclusive mode, if possible. The exclusive mode keeps other programs or
+    threads from accessing the file until the current process closes the file.]
+
+ * After a file is successfully opened, fopen() returns a file pointer, which
+   the other I/O functions can then use to specify the file.
+   The file pointer is of type pointer-to-FILE.
+   The file pointer doesn't point to the actual file. Instead, it points to a
+   structure containing information about the file, including information about
+   the buffer used for the file's I/O.
+ * fopen() returns a null pointer if the file cannot be opened.
+
+ * The getc() and putc() functions work similar to the getchar() and putchar()
+   functions.
+   getchar() is equivalent to getc(stdin) and putchar(c) is equivalent to
+   putc(c, stdout).
+ * Just like getchar(), getc() also returns EOF when it encounters end-of-file.
+
+ * The fclose() function closes the file identified by the file pointer,
+   flushing buffers as needed.
+ * fclose() returns 0 if the file is closed successfully, and EOF if not.
 
  */
