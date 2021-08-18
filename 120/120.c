85d1447450e225981ada00ada68597dd3f822b98
KushagrJ <kushagr.j@gmail.com>
2021-08-18 12:44:17 +0530

    Update C-01

--- 120/120.c
+++ 120/120.c
@@ -1,5 +1,6 @@
 // C17 Standard
 
+
 #include <stdio.h>
 
 
@@ -49,6 +50,7 @@ int main(void)
  * The preprocessor doesn't do any calculations. It just makes substitutions
    very literally.
 
+
  * Each #define directive has three parts. The first part is #define itself.
    The second part is the chosen abbreviation, known as a macro. The macro name
    must conform to the same naming rules as C identifiers follow.
@@ -69,7 +71,7 @@ int main(void)
    #define KJ "Program\
    ming is fun!"
    #define FOUR TWO*TWO   // Macro-nesting isn't supported by all compilers.
-   #define PRINT_4 printf("%d\n", FOUR);
+   #define PRINT_4 printf("%d\n", FOUR)
  * String literals using #define should be used carefully with printf(),
    scanf(), etc., as unknown conversion specifiers (such as %b) invoke undefined
    behaviour.
@@ -80,6 +82,65 @@ int main(void)
             ...
  * String literals in general should be used carefully, as unknown escape
    sequences (such as \c) also invoke undefined behaviour.
+
+ * The body of a macro is considered to be a string of tokens rather than a
+   string of individual characters by the preprocessor.
+   For eg., in #define FOUR 2*2, there is one token (2*2), but in
+            #define SIX 2 * 3, there are 3 tokens (2, * and 3).
+   [The character string interpretation views the spaces as part of the body,
+    whereas the token interpretation views the spaces as separators between the
+    tokens of the body]
+   [The C compiler takes a more complex view of tokens than the preprocessor
+    does. The compiler understands the rules of C and doesn’t necessarily
+    require spaces to separate tokens. For eg., the C compiler would view 2*2 as
+    three tokens because it recognizes that each 2 is a constant and that * is
+    an operator.]
+ * Thus, #define SIX 2 * 3 and #define SIX 2    *    3 are the same, but
+   #define SIX 2*3 is different.
+   Redefinition without first using #undef is allowed only if the new definition
+   duplicates the old, as per the C Standard.
+
+ * FOUR, SIX, etc. are known as object-like macros, because they resemble data
+   objects when used.
+ * By using arguments with #define, the user can create function-like macros
+   that look and act much like functions.
+   For eg., #define SQUARE(X) X*X
+            #define PR(X) printf("The result is %d.\n", X)
+ * A macro argument doesn't work exactly like a function argument.
+   For eg., (a) If SQUARE() were a function, then SQUARE(5+2) would've returned
+                49, but since SQUARE() is a macro defined as X*X, SQUARE(5+2) is
+                replaced by 5+2*5+2, which the compiler later evaluates to 17.
+                This can be solved by using #define SQUARE(X) (X)*(X).
+            (b) Now, since SQUARE() is defined as (X)*(X), 100/SQUARE(2) would
+                be replaced by 100/(2)*(2), which would later evaluate to 100.
+                Thus, to solve this as well, SQUARE(X) should be defined as
+                ((X)*(X)).
+            (c) Finally, SQUARE(++x) would be replaced by ((++x)*(++x)) and
+                invoke undefined behaviour, no matter how many parentheses are
+                used in the body. [Similarly for others]
+                Thus, increment/decrement operators shouldn't be used with
+                macros.
+
+ * For eg., #define PSQR(X) printf("The square of X is %d.\n", ((X)*(X)))
+   PSQR(8); would print The square of X is 64., as the X inside double quotes is
+   treated as ordinary text, not as a token that can be replaced. One way to
+   print The square of 8 is 64. instead of The square of X is 64. is by using
+   #define PSQR(X) printf("The square of %d is %d.\n", X, ((X)*(X)))
+ * But, this can also be done by another method, i.e. by using the # operator.
+   Within the body of a function-like macro, the # symbol becomes a
+   preprocessing operator that converts tokens into strings. For eg., if X is a
+   macro parameter, then #X is that parameter converted to "X". This process is
+   called stringizing. Thus, in order to print The square of 8 is 64.,
+   #define PSQR(X) printf("The square of " #X " is %d.\n", ((X)*(X))) can also
+   be used. PSQR(8); will now be replaced by
+   printf("The square of " "8" " is %d.\n", ((8)*(8))); in the program.
+
+ * The ## operator combines two tokens into a single token. It can be used in
+   the bodies of object-like and function-like macros.
+   For eg., #define KJ(n) x ## n
+            ...
+            int KJ(1) = 22;   // becomes int x1 = 22;
+            ...
 
    https://gcc.gnu.org/onlinedocs/cpp/Macros.html#Macros - See this as well
 
