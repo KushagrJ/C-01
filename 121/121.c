955605da1c518061729a7d90101405c1c24b6c65
KushagrJ <kushagr.j@gmail.com>
2021-08-28 16:18:56 +0530

    Update C-01

--- 121/121.c
+++ 121/121.c
@@ -16,7 +16,19 @@ int main(void)
 
 /* Trivia
 
- *
+ * The #undef directive undefines a given #define.
+   For eg., #define LIMIT 400
+            ...
+            #undef LIMIT
+            #define LIMIT 500
+ * Even if a macro isn't defined in the first place, it is still valid to
+   undefine it. This is done mostly when the user is unsure whether a macro name
+   has been used previously, and they undefine it to be on the safer side.
+ * The scope of a #define macro extends from the point it is declared in a file
+   until it is the subject of an #undef directive or until the end of file,
+   whichever comes first.
+ * Undefining predefined macros such as __DATE__ and __FILE__ invokes undefined
+   behaviour.
 
    https://gcc.gnu.org/onlinedocs/cpp/Macros.html#Macros - See this as well
 
